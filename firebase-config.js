f4c21204fbbef4176c30d9945048db346671fd5d
Tzusee <tonyprojects0504@gmail.com>
2025-11-22 23:31:05 +0100

    Update TrackFlix

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// OVAJ FILE NE IDE NA GITHUB
+
+window.TRACKFLIX_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBmBLvK8j6fVFV7vcAfAz8rE-5lhCfyqSA",
+  authDomain: "trackflix-350d9.firebaseapp.com",
+  projectId: "trackflix-350d9",
+  storageBucket: "trackflix-350d9.firebasestorage.app",
+  messagingSenderId: "564135421634",
+  appId: "1:564135421634:web:79ac49c50f629fb1dbbd5b",
+  measurementId: "G-6YBFZ8LQNC"
+};
